5780d1e89be45a9c5fcb72f5ccbc7d338192ad61
Ryma Djoudad <zenyod85@gmail.com>
2026-02-15 15:29:17 -0500

    Green Alien Points

--- alien-colors.py
+++ alien-colors.py
@@ -2,4 +2,9 @@ alien_color = "green"
 
 if alien_color == "green":
     print("Congratulations! You just earned 5 points.")
-    
+elif alien_color == "yellow":
+    print("Congratulations! You just earned 10 points.")
+elif alien_color == "red":
+    print("Congratulations! You just earned 15 points.")
+else: 
+    print("Points undetermined.")
